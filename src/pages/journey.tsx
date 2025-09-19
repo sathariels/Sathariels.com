b589a059a976b1c380bc43aa685a3d3120840785
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-09-19 11:15:20 -0400

    Update journey.tsx

--- src/pages/journey.tsx
+++ src/pages/journey.tsx
@@ -1,4 +1,5 @@
 import { ImageWithFallback } from "../components/figma/ImageWithFallback";
+import Image1 from 'figma:asset/Screenshot 2025-09-19 110640.png';
 
 const journeyData = [
   {
@@ -8,7 +9,7 @@ const journeyData = [
     date: "Early 2020",
     complexity: "Beginner",
     videoUrl: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    thumbnail: "https://images.unsplash.com/photo-1665217101903-923e53b74b77?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxhbmltYXRpb24lMjBiZWdpbm5lciUyMGJvdW5jaW5nJTIwYmFsbHxlbnwxfHx8fDE3NTc1NjcwMzB8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral",
+    thumbnail: Image1,
     side: "right"
   },
   {
