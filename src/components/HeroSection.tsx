24f41a573f0cfe1f2efe1de9aec2000774e8d68b
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-22 17:20:57 -0500

    Update image source in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,7 +21,7 @@ export default function HeroSection() {
         whileHover="hover"
       >
         <Image
-          src="/svgs/Vector3.svg"
+          src="/svgs/Vector2.svg"
           alt="Nithilan Kumaran"
           width={150}
           height={150}
