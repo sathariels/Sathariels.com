e8e9d77c6b4a4d4a1e6605d7c4818e34a0a5c827
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-22 16:20:28 -0500

    Update person image source in siteContent

--- src/lib/constants/siteContent.ts
+++ src/lib/constants/siteContent.ts
@@ -10,6 +10,6 @@ export const aboutDescription =
   "Lavanya Yasmeen is a pixel perfectionist graphic designer, renowned for transforming dreams into digital magic. Based in Indonesia, she crafts visually stunning designs that captivate audiences worldwide.";
 
 export const personImage = {
-  src: "/person2.jpg",
+  src: "/SatharielsPicture.jpg",
   alt: "Photo by Cherry Ann Gonzales: https://www.pexels.com/photo/colorful-portrait-of-woman-with-sunglasses-and-balls-33819483/",
 };
