5aa607b9d55013b43e76d469c49da3d72a5718ed
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-23 01:03:47 -0500

    Update socials.ts

--- src/lib/constants/socials.ts
+++ src/lib/constants/socials.ts
@@ -3,4 +3,5 @@ export const socials = {
   linkedin: "https://www.linkedin.com/in/nithilan-kumaran",
   twitter: "https://x.com/sathariel",
   instagram: "https://instagram.com/nithilan.3",
+  Medium: "https://medium.com/@Sathariels",
 };
