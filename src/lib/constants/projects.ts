c0368ddc87fef2a790483864e98845ea82b0b1d9
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-22 21:37:57 -0500

    Update project image source for 'Projects'

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -1,7 +1,7 @@
 export type Project = { name: string; imgSrc: string };
 
 export const projects: Project[] = [
-  { name: "Projects", imgSrc: "/projects/ShadowSlaveMask(2).png" },
+  { name: "Projects", imgSrc: "/projects/PurpleTreeMidjourney.png" },
   { name: "NumberOne", imgSrc: "/projects/work-2.jpg" },
   { name: "Yasmeen", imgSrc: "/projects/work-3.jpg" },
   { name: "Inspo", imgSrc: "/projects/work-4.jpg" },
