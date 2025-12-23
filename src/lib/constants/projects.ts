644b6aa0582793ff718171f00bca02e670467a0a
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-23 00:38:24 -0500

    Update projects.ts

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -4,5 +4,5 @@ export const projects: Project[] = [
   { name: "Projects", imgSrc: "/projects/PurpleTreeMidjourney.png" },
   { name: "Shadow Slave Mask", imgSrc: "/projects/ShadowSlave.png" },
   { name: "Flower Mask", imgSrc: "/projects/FlowerMask.png" },
-  { name: "VacuumCore", imgSrc: "/projects/BlackHole.png" },
+  { name: "Vacuum Core", imgSrc: "/projects/BlackHole.png" },
 ];
