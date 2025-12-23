f8edea9451bdba5ebdbaa9ccce202abca00b4ff3
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-22 21:16:45 -0500

    Update image source for Parzival project

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -1,7 +1,7 @@
 export type Project = { name: string; imgSrc: string };
 
 export const projects: Project[] = [
-  { name: "Parzival", imgSrc: "/projects/work-1.jpg" },
+  { name: "Parzival", imgSrc: "/projects/ShadowSlaveMask.png" },
   { name: "NumberOne", imgSrc: "/projects/work-2.jpg" },
   { name: "Yasmeen", imgSrc: "/projects/work-3.jpg" },
   { name: "Inspo", imgSrc: "/projects/work-4.jpg" },
