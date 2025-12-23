0b57c3a70140f67b42ec85f12d02c41fe67e3b48
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-23 01:05:34 -0500

    Remove Medium link from socials
    Removed Medium link from socials constants.

--- src/lib/constants/socials.ts
+++ src/lib/constants/socials.ts
@@ -3,5 +3,4 @@ export const socials = {
   linkedin: "https://www.linkedin.com/in/nithilan-kumaran",
   twitter: "https://x.com/sathariel",
   instagram: "https://instagram.com/nithilan.3",
-  Medium: "https://medium.com/@Sathariels",
 };
