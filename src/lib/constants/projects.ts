3d24607ec019492dd2c20663b2309f70a29a3b76
Nithilan Kumaran <126580000+sathariels@users.noreply.github.com>
2025-12-22 21:43:41 -0500

    Replace 'NumberOne' with 'Shadow Slave Mask' project

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -2,7 +2,7 @@ export type Project = { name: string; imgSrc: string };
 
 export const projects: Project[] = [
   { name: "Projects", imgSrc: "/projects/PurpleTreeMidjourney.png" },
-  { name: "NumberOne", imgSrc: "/projects/work-2.jpg" },
+  { name: "Shadow Slave Mask", imgSrc: "/projects/ShadowSlave.jpg" },
   { name: "Yasmeen", imgSrc: "/projects/work-3.jpg" },
   { name: "Inspo", imgSrc: "/projects/work-4.jpg" },
 ];
