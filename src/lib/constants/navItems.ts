e497163a67bb0f3f2bc44450ed856299a3d2758f
SylvieSancto <122183650+SylvieSancto@users.noreply.github.com>
2026-01-03 23:45:27 -0500

    fix: Update About navigation link to root URL

--- src/lib/constants/navItems.ts
+++ src/lib/constants/navItems.ts
@@ -1,5 +1,5 @@
 export const navItems = [
   { href: '/projects', label: 'View projects', title: 'Projects' },
   { href: '/contact', label: 'Contact information', title: 'Contact' },
-  { href: '/#about', label: 'About me', title: 'About' } // Ensure About links back to home anchor
+  { href: '/', label: 'About me', title: 'About' }
 ];
